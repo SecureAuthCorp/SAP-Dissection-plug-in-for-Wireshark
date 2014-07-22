955d00fbb4dada9d4b7e5a59ae1d6e3f6b77b377
Martin Gallo <mgallo@coresecurity.com>
2014-07-22 19:24:36 -0300

    Code cleanup and license notice format update

--- saphelpers.h
+++ saphelpers.h
@@ -1,21 +1,22 @@
 /*
-## ===========
-## SAP Dissector Plugin for Wireshark
-##
-## Copyright (C) 2014 Core Security Technologies
-##
-## The plugin was designed and developed by Martin Gallo from the Security Consulting Services team of Core Security Technologies.
-##
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License
-## as published by the Free Software Foundation; either version 2
-## of the License, or (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##==============
+# ===========
+# SAP Dissector Plugin for Wireshark
+#
+# Copyright (C) 2014 Core Security Technologies
+#
+# The plugin was designed and developed by Martin Gallo from the Security
+# Consulting Services team of Core Security Technologies.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ==============
 */
 
 #ifndef __PACKET_SAPHELPERS_H__
