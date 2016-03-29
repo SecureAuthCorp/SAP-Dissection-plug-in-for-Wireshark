f4f128b927cbafcc8c9155aa3aeb9f5363fb08e5
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-03-29 21:15:02 +0200

    sapdiag: Add modelines info

--- src/packet-sapdiag.c
+++ src/packet-sapdiag.c
@@ -3571,3 +3571,15 @@ proto_reg_handoff_sapdiag(void)
 	range_foreach(sapdiag_port_range, range_add_callback);
 }
 
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
