deb7e46aa0bd9462db6b4a12bd6b1bb343e9ea1e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-03-30 17:01:51 +0200

    sapenqueuse: Add modelines info

--- src/packet-sapenqueue.c
+++ src/packet-sapenqueue.c
@@ -565,3 +565,16 @@ proto_reg_handoff_sapenqueue(void)
 	 * Service (32NN/tcp). */
 	heur_dissector_add("sapni", dissect_sapenqueue_heur, "SAP Enqueue Protocol", "sapenqueue", proto_sapenqueue, HEURISTIC_ENABLE);
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
