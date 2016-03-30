bac00f8efa53bd3cd6a189f8b3c5ff920d232501
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-03-30 18:52:25 +0200

    saprouter: Add modelines info

--- src/packet-saprouter.c
+++ src/packet-saprouter.c
@@ -807,3 +807,15 @@ proto_reg_handoff_saprouter(void)
 
 }
 
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
