67740c5653b0e79ba2154d04c96be90ffdd65d82
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-03-30 18:43:50 +0200

    saprfc: Add modelines info

--- src/packet-saprfc.c
+++ src/packet-saprfc.c
@@ -1284,3 +1284,16 @@ proto_reg_handoff_saprfc(void)
 	saprfc_port_range = range_copy(global_saprfc_port_range);
 	range_foreach(saprfc_port_range, range_add_callback);
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
