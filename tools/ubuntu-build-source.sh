fad91278eb59db548d1bd65486eb41c62f5201b7
Martin Gallo <mgallo@coresecurity.com>
2016-11-09 11:34:07 -0800

    Build the entire wireshark if required

--- tools/ubuntu-build-source.sh
+++ tools/ubuntu-build-source.sh
@@ -3,5 +3,10 @@
 set -e
 
 # Build tshark and plugins
-mkdir -p build && cd build && cmake ${HOME}/wireshark-$WIRESHARK_BRANCH && make -j3 plugins tshark sap
-export PATH=${PATH}:${HOME}/wireshark-$WIRESHARK_BRANCH/build
+mkdir -p build && cd build && cmake ${HOME}/wireshark-${WIRESHARK_BRANCH} && make -j3 tshark sap
+
+if [ ${BUILD_WIRESHARK} == "yes" ]; then
+    make -j3 wireshark
+fi
+
+export PATH=${PATH}:${HOME}/wireshark-${WIRESHARK_BRANCH}/build
