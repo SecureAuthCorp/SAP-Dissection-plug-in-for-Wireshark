ec355dc284a812091588ecc68948d9d8b6e13021
Martin Gallo <mgallo@coresecurity.com>
2016-11-09 16:42:53 -0800

    Fix check

--- tools/ubuntu-build-source.sh
+++ tools/ubuntu-build-source.sh
@@ -23,8 +23,8 @@ set -e
 # Build tshark and plugins
 mkdir -p build && cd build && cmake ${HOME}/wireshark-${WIRESHARK_BRANCH} && make -j3 tshark sap
 
-if [ ${BUILD_WIRESHARK} == "yes" ]; then
-    make -j3 wireshark
+if [ "${BUILD_WIRESHARK}" == "yes" ]; then
+    make -j3 wireshark;
 fi
 
 export PATH=${PATH}:${HOME}/wireshark-${WIRESHARK_BRANCH}/build
