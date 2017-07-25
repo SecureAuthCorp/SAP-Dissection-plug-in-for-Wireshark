4f6becd58319e980e0361481c9cbf68784a20128
Martin Gallo <mgallo@coresecurity.com>
2017-07-25 00:54:08 -0300

    Testing Router packet fields

--- tests/saprouter_test.py
+++ tests/saprouter_test.py
@@ -30,6 +30,36 @@ from basetestcase import WiresharkTestCase
 
 class WiresharkSAPRouterTestCase(WiresharkTestCase):
 
+    route_mapping = {
+        "type": "type",
+        "version": ("version", int),
+        "route_ni_version": ("niversion", int),
+        "route_entries": ("entries", int),
+        "route_talk_mode": ("talkmode", int),
+        "route_rest_nodes": ("restnodes", int),
+        "route_length": ("routelength", int),
+        "route_offset": ("routeoffset", int),
+    }
+
+    admin_mapping = {
+        "type": "type",
+        "version": ("niversion", int),
+        "adm_command": ("command", int),
+        "adm_password": "password",
+        "adm_client_count": ("client_count", int),
+        #"adm_client_ids": "client_id",
+        "adm_address_mask": "address_mask",
+    }
+
+    control_mapping = {
+        "type": "type",
+        "version": ("niversion", int),
+        "opcode": ("opcode", int),
+        "return_code": ("returncode", int),
+        "control_text_length": ("controllength", int),
+        #"control_text_value": "controltext",
+    }
+
     def test_saprouter_dissection(self):
         """Test dissection of a basic SAP Router packet. """
         pkt = Ether()/IP()/TCP(dport=3299)/SAPNI()/SAPRouter()
@@ -42,6 +72,7 @@ class WiresharkSAPRouterTestCase(WiresharkTestCase):
 
     def test_saprouter_route_dissection(self):
         """Test dissection of a SAP Router route request."""
+
         # Build the Route request packet
         router_string = [SAPRouterRouteHop(hostname="1.2.3.4",
                                            port=1234),
@@ -62,18 +93,89 @@ class WiresharkSAPRouterTestCase(WiresharkTestCase):
         packet = self.get_capture(pkt)[0]
 
         self.assertIn('sapni', packet)
-        self.assertEqual(int(packet['sapni'].length), 60)
+        self.assertEqual(int(packet['sapni'].length), len(p))
         self.assertIn('saprouter', packet)
-
-        self.assertEqual(p.type, packet['saprouter'].type)
-        self.assertEqual(p.version, int(packet['saprouter'].version))
-        self.assertEqual(p.route_ni_version, int(packet['saprouter'].niversion))
-        self.assertEqual(p.route_entries, int(packet['saprouter'].entries))
-        self.assertEqual(p.route_talk_mode, int(packet['saprouter'].talkmode))
-        self.assertEqual(p.route_rest_nodes, int(packet['saprouter'].restnodes))
-        self.assertEqual(p.route_length, int(packet['saprouter'].routelength))
-        self.assertEqual(p.route_offset, int(packet['saprouter'].routeoffset))
+        self.assert_fields(p, packet["saprouter"], self.route_mapping)
         self.assertEqual("Route password found", packet['saprouter']._ws_expert_message)
+
+    def test_saprouter_admin_info_request_dissection(self):
+        """Test dissection of a SAP Router admin info request."""
+
+        # Build the Admin packet
+        p = SAPRouter(type=SAPRouter.SAPROUTER_ADMIN,
+                      version=2,
+                      adm_command=2,
+                      adm_password="SomePa$$w0rd")
+
+        pkt = Ether()/IP()/TCP(sport=3299, dport=9999)/SAPNI()/p
+
+        packet = self.get_capture(pkt)[0]
+
+        self.assertIn('sapni', packet)
+        self.assertEqual(int(packet['sapni'].length), len(p))
+        self.assertIn('saprouter', packet)
+        self.assert_fields(p, packet["saprouter"], self.admin_mapping)
+        self.assertEqual("Info password found", packet['saprouter']._ws_expert_message)
+
+    def test_saprouter_admin_cancel_trace_route_dissection(self):
+        """Test dissection of a SAP Router admin cancel route and trace route request."""
+
+        for command in [6, 12, 13]:
+            # Build the Admin packet
+            p = SAPRouter(type=SAPRouter.SAPROUTER_ADMIN,
+                          version=2,
+                          adm_command=command,
+                          adm_client_count=2,
+                          adm_client_ids=[1,2])
+
+            pkt = Ether()/IP()/TCP(sport=3299, dport=9999)/SAPNI()/p
+
+            packet = self.get_capture(pkt)[0]
+
+            self.assertIn('sapni', packet)
+            self.assertEqual(int(packet['sapni'].length), len(p))
+            self.assertIn('saprouter', packet)
+            self.assert_fields(p, packet["saprouter"], self.admin_mapping)
+
+    def test_saprouter_admin_peer_trace_dissection(self):
+        """Test dissection of a SAP Router admin set/clear peer trace."""
+
+        for command in [10, 11]:
+            # Build the Admin packet
+            p = SAPRouter(type=SAPRouter.SAPROUTER_ADMIN,
+                          version=2,
+                          adm_command=command,
+                          adm_address_mask="0.0.0.0")
+
+            pkt = Ether()/IP()/TCP(sport=3299, dport=9999)/SAPNI()/p
+
+            packet = self.get_capture(pkt)[0]
+
+            self.assertIn('sapni', packet)
+            self.assertEqual(int(packet['sapni'].length), len(p))
+            self.assertIn('saprouter', packet)
+            self.assert_fields(p, packet["saprouter"], self.admin_mapping)
+
+    def test_saprouter_control_dissection(self):
+        """Test dissection of a SAP Router control."""
+
+        # Build the Control packet
+        p = SAPRouter(type=SAPRouter.SAPROUTER_CONTROL,
+                      version=2,
+                      opcode=1,
+                      return_code=-99,
+                      control_text_length=8,
+                      control_text_value="SOMEDATA")
+
+        pkt = Ether()/IP()/TCP(sport=3299, dport=9999)/SAPNI()/p
+
+        packet = self.get_capture(pkt)[0]
+
+        print(dir(packet["saprouter"]))
+        self.assertIn('sapni', packet)
+        self.assertEqual(int(packet['sapni'].length), len(p))
+        self.assertIn('saprouter', packet)
+        self.assert_fields(p, packet["saprouter"], self.control_mapping)
 
 
 def suite():
